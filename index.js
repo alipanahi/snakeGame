986de196c1f602e671558e2020c5ebe2f62a1204
Panahi <apanahi@nfb.org>
2025-09-19 10:00:28 -0400

    debugging

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
 const grid = document.getElementById('grid')
 let scoreDiv = document.getElementById('score')
 const start = document.getElementById('start')
-let currentSnake = [49,29,9] //the starts from top toward down
+const error = document.getElementById('error')
+let currentSnake = [49,29,9] //the starts from top toward down (squar index)
 const squares = [] // array of all the ground boxes
 const width = 20 // the width of our game
 let timer  = 0 // default timer for snake speed
@@ -12,9 +14,9 @@ let direction = width // by default the snake goes down
 let appleIndex = 0
 let dangerIndex = 0
 let score = 0
-const dangerLimit = 5 //every 5 scores, one danger point will be added
+const dangerLimit = 2 //every 2 scores, one danger point will be added
 let dangerList = []
-let difficulty = false // in hard difficulty the apple gets disapper
+let difficulty = false // in hard difficulty the apple gets disapper after specific time(difficultyTimer)
 
 for(let i = 0; i < 400; i++){
     let squar = document.createElement('div')
@@ -30,15 +32,21 @@ currentSnake.forEach(index => squares[index].classList.add('snake'))
 squares[currentSnake[0]].classList.add('snake-head')
 start.addEventListener('click',startGame)
 
+
 function startGame(){
+    error.textContent = ''
+    //reset all the classes from prevoiuse game
     currentSnake.forEach(index => squares[index].classList.remove('snake'))
     squares[currentSnake[0]].classList.remove('snake-head')
     squares[appleIndex].classList.remove('apple')
     dangerList.forEach(index => squares[index].classList.remove('danger'))
     if(document.getElementById('hard').checked){
         difficulty = true
+    }else{
+        difficulty = false
     }
     clearInterval(timer)
+    //set all default values
     currentSnake = [49,29,9]
     dangerList = []
     intervalTime = 1000
@@ -48,24 +56,34 @@ function startGame(){
     currentSnake.forEach(index => squares[index].classList.add('snake'))
     squares[currentSnake[0]].classList.add('snake-head')
     generateApple()
+    
     timer = setInterval(move,intervalTime)
 }
 function move()
 {
-    if((currentSnake[0] + width >= width*width && direction === width) ||
-        (currentSnake[0] % width === width-1 && direction === 1) ||
+    /*
+    0 ... 19
+    20 ... 39
+    40 ... 59 
+    */
+    if((currentSnake[0] + width >= width*width && direction === width) ||//check if snake reachs the most bottom row
+        (currentSnake[0] % width === width-1 && direction === 1) ||//check if snake reaches the most right column
         (currentSnake[0] % width === 0 && direction === -1) ||
-        (currentSnake[0] - width < 0 && direction === -width) ||
+        (currentSnake[0] - width < 0 && direction === -width) ||//check if snake reaches the first row
         squares[currentSnake[0] + direction].classList.contains('snake') ||
         squares[currentSnake[0] + direction].classList.contains('danger')
     ){
+        error.textContent = 'Game over !!!'
+        //return
         return clearInterval(timer)
     }
-    tail = currentSnake.pop() //remove first index of currentSnake array
+    tail = currentSnake.pop() //remove last index of currentSnake array
     squares[tail].classList.remove('snake') // remove the background from tail
     squares[currentSnake[0]].classList.remove('snake-head')
-    currentSnake.unshift(currentSnake[0] + direction) //add to snake array according to direction
-    
+    currentSnake.unshift(currentSnake[0] + direction) //add to snake array according to direction as first index
+    squares[currentSnake[0]].classList.add('snake')
+    squares[currentSnake[0]].classList.add('snake-head')
+
     if(squares[currentSnake[0]].classList.contains('apple')){//hit the apple
         squares[currentSnake[0]].classList.remove('apple')
         squares[currentSnake[0]].style=''
@@ -80,29 +98,40 @@ function move()
         if(score % dangerLimit === 0){//for every 5 scores, add one danger point
             generateDangerPoint()
         }
+        
         intervalTime = intervalTime * 0.9
         clearInterval(timer)
-        timer = setInterval(move,intervalTime)
+        timer = setInterval(move,intervalTime)//recursive call
         
         scoreDiv.textContent = score;
         
     }
-    squares[currentSnake[0]].classList.add('snake')
-    squares[currentSnake[0]].classList.add('snake-head')
+    
 }
+let lastDirection = width
 function controller(e){
+    
     if(e.keyCode === 37){//turn the snake to left
-        direction = -1
+        if(lastDirection != 1){
+            direction = -1
+        }
     }
     else if(e.keyCode === 38){//turn the snake to top
-        direction = -width
+        if(lastDirection != width){
+            direction = -width
+        }
     }
     else if(e.keyCode === 39){//turn the snake to right
-        direction = 1
+        if(lastDirection != -1){
+            direction = 1
+        }
     }
     else if(e.keyCode === 40){//turn the snake to down
-        direction = width
+        if(lastDirection != -width){
+            direction = width
+        }
     }
+    lastDirection = direction
 }
 document.addEventListener('keydown',controller)
 
@@ -118,8 +147,8 @@ function generateApple(){
 function generateDangerPoint(){
     do{
         dangerIndex = Math.floor(Math.random() * 400 )
-    }while(squares[dangerIndex].classList.contains('snake') && 
-            squares[dangerIndex].classList.contains('apple') && 
+    }while(squares[dangerIndex].classList.contains('snake') || 
+            squares[dangerIndex].classList.contains('apple') || 
             squares[dangerIndex].classList.contains('danger')
         )
     squares[dangerIndex].classList.add('danger')
